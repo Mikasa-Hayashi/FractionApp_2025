3c99492cd4880b23a23f093d24ad372fd4bee281
Ivan <ivandenisovich2005@gmail.com>
2025-09-11 18:56:00 +0300

    chore: added tests for toString method

--- test/TestToString.java
+++ test/TestToString.java
@@ -5,10 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class TestToString {
     @Test
-    void simpleTest() {
+    void irreducibleFraction() {
         String expString = "1/2";
         ProperFraction fraction = new ProperFraction(1, 2);
         String actualString = fraction.toString();        
+        Assertions.assertEquals(expString, actualString);
+    }
+
+    @Test
+    void reducibleFraction() {
+        String expString = "1/2";
+        ProperFraction fraction = new ProperFraction(2, 4);
+        String actualString = fraction.toString();
+        Assertions.assertEquals(expString, actualString);
+    }
+
+    @Test
+    void numeratorIsZero() {
+        String expString = "0";
+        ProperFraction fraction = new ProperFraction(0, 2);
+        String actualString = fraction.toString();
         Assertions.assertEquals(expString, actualString);
     }
 }
